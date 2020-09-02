10cb817896e0e31fc21df9282353d9d030f28123
Dziyana Bahdanava <dziyana_bahdanava@epam.com>
2020-09-02 17:38:32 +0300

    Add integration tests

---
+++ src/test/java/com/epam/ms/repository/UserProfileRepositoryIntegrationTest.java
@@ -0,0 +1,66 @@
+package com.epam.ms.repository;
+
+import com.epam.ms.repository.domain.Goal;
+import com.epam.ms.repository.domain.PhysicalActivity;
+import com.epam.ms.repository.domain.UserProfile;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UserProfileRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserProfileRepository repository;
+
+    @Test
+    public void whenFindById_thenReturnUserProfile() {
+        // given
+        UserProfile profile = createDefaultProfile();
+        UserProfile persisted = entityManager.persist(profile);
+        entityManager.flush();
+
+        // when
+        Optional<UserProfile> found = repository.findById(persisted.getId());
+
+        // then
+        assertThat(found.isPresent()).isTrue();
+        assertThat(found.get().getId()).isEqualTo(persisted.getId());
+    }
+
+    @Test
+    public void whenFindByName_thenReturnEmployee() {
+        // given
+        UserProfile profile = createDefaultProfile();
+        entityManager.persist(profile);
+        entityManager.flush();
+
+        // when
+        List<UserProfile> found = (List<UserProfile>) repository.findAll();
+
+        // then
+        assertThat(found.get(0).getAge()).isEqualTo(25);
+    }
+
+    private UserProfile createDefaultProfile() {
+        UserProfile profile = new UserProfile();
+        profile.setPhysicalActivity(PhysicalActivity.LOW_ACTIVITY);
+        profile.setGoal(Goal.BODY_RELIEF);
+        profile.setWeight(80);
+        profile.setHeight(175);
+        profile.setAge(25);
+        return profile;
+    }
+}
