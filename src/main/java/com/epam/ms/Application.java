d6fc5ca626e625bb2aa2676a22040a4ae4283e68
Dziyana Bahdanava <dziyana_bahdanava@epam.com>
2020-09-23 22:41:55 +0300

    Add Consul

--- src/main/java/com/epam/ms/Application.java
+++ src/main/java/com/epam/ms/Application.java
@@ -2,11 +2,14 @@ package com.epam.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
